3e0258839f86ae56ce2bad20726804c66790b5f5
NiklasSod <nma.prob@gmail.com>
2026-08-12 13:09:10 +0200

    config styled components

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  border: {
+    color_light: '#E2E8F0',
+  },
+  color: {
+    black: '#0F172A',
+    green: '#10B981',
+    gray: '#64748B',
+  },
+  hover: {
+    green: '#37e5ab',
+    gray: '#9aa9be',
+  }
+};
